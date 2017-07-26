0cd91099f7b131cd55ed1d2a432d75f633bb72f1
moonlightlane <wangzichao6@gmail.com>
2017-07-26 14:47:49 -0500

    minor bug fix

--- seq2seq.py
+++ seq2seq.py
@@ -964,7 +964,7 @@ if use_cuda:
     encoder2 = encoder2.cuda()
     attn_decoder1 = attn_decoder1.cuda()
 
-trainIters(encoder1, encoder2, attn_decoder1, 75000, print_every=5000)
+trainIters(encoder1, encoder2, attn_decoder1, embeddings_index, 75000, print_every=5000)
 
 ######################################################################
 #
