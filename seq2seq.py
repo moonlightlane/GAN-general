ded7373975e8c67f879900463efd2b595f4c9b99
moonlightlane <wangzichao6@gmail.com>
2017-07-27 17:41:57 -0500

    minor fix

--- seq2seq.py
+++ seq2seq.py
@@ -703,7 +703,7 @@ if use_cuda:
 ######### start training
 trainIters(encoder1, encoder2, attn_decoder1, embeddings_index, word2index,
             path_to_loss_f, path_to_sample_out_f, path_to_exp_out,
-            75000, print_every=5000)
+            75000, print_every=10)
 
 # save the final model
 torch.save(encoder1, path_to_exp_out+'/encoder1.pth')
