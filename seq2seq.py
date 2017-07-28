5f9a18f6cc30707870e5fae07930aa4988825a1a
moonlightlane <wangzichao6@gmail.com>
2017-07-27 21:23:16 -0500

    minor update

--- seq2seq.py
+++ seq2seq.py
@@ -684,8 +684,8 @@ print('')
 
 
 ######### set up model
-hidden_size1 = 256
-hidden_size2 = 256
+hidden_size1 = 64
+hidden_size2 = 64
 # context encoder
 encoder1 = EncoderRNN(embeddings_size, hidden_size1)
 # answer encoder
